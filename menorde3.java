1a09cdb66701e72e647e78ffc355abc5b6dbd6d8
TiagoLima2017 <32169068+TiagoLima2017@users.noreply.github.com>
2018-01-26 16:08:20 +0000

    Update menorde3.java

--- menorde3.java
+++ menorde3.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Funçao2 {
     private static Scanner lerS;
 	static int menorde3 (int X, int Y, int Z)
-    {if (X < Y)
-        X = Y;
-     if (X < Z)
-         X = Z;
-    return X;}
-    static int menorde3 (int X, int Y, int Z)
     {if (X > Y)
         X = Y;
      if (X > Z)
+         X = Z;
+    return X;}
+    static int menorde31 (int X, int Y, int Z)
+    {if (X < Y)
+        X = Y;
+     if (X < Z)
          X = Z;
     return X;}
     public static void main (String [] args) throws Exception{
